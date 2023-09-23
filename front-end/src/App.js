895f43fe0b3e0f13ac9d1d715b35725ffc716be5
Jared Feng <fengdongliang002@gmail.com>
2023-09-24 00:20:23 +1000

    day-1

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Home />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
